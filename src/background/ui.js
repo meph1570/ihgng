53f88f7bcbd01ad36d17e7a9a45de2f0a1e2e4bb
mephisto <mephisto@cpc.to>
2017-09-04 22:54:54 +0200

    Refactored tab creation & message sending upon creation

--- src/background/ui.js
+++ src/background/ui.js
@@ -23,12 +23,41 @@ function getPanelTab(panelUrl) {
 }
 
 
+function createTabAndSendMessage(tabParams, message) {
+    browser.tabs.create(tabParams).then(
+        (tab) => {
+            let sendMessage = () => browser.tabs.sendMessage(tab.id, message);
+
+            if (tab.status !== "complete") {
+                let listener = function (tabId, changeInfo, tab) {
+                    if (tabId === tab.id && changeInfo.status === "complete") {
+                        sendMessage();
+                        browser.tabs.onUpdated.removeListener(listener);
+
+                    }
+                };
+                browser.tabs.onUpdated.addListener(listener);
+            }
+            else if (tab.status === "complete") {
+                sendMessage();
+            }
+        }
+    );
+}
+
+
 function openPanel() {
     let panelUrl = browser.extension.getURL("panel/panel.html");
 
     getPanelTab(panelUrl).then(
         (tab) => browser.tabs.update(tab.id, {active: true}),
-        () => browser.tabs.create({url: panelUrl, active: true})
+        () => {
+            //browser.tabs.create({url: panelUrl, active: true}).then()
+            createTabAndSendMessage({url: panelUrl, active: true}, {
+                action: "pause-changed",
+                paused: ihgng.downloadList.paused
+            });
+        }
     );
 }
 
@@ -36,6 +65,12 @@ function openPanel() {
 function openLinkSelect(links) {
     let panelUrl = browser.extension.getURL("link_select/link_select.html");
 
+    createTabAndSendMessage({url: panelUrl, active: true}, {
+        action: "set-links",
+        links: links
+    });
+
+    /*
     browser.tabs.create({url: panelUrl, active: true}).then(
         (tab) => {
             let sendLinks = () => browser.tabs.sendMessage(tab.id, {
@@ -57,11 +92,12 @@ function openLinkSelect(links) {
             }
 
         }
-    );
+    );*/
 }
 
 
 export {
+    getPanelTab,
     openLinkSelect,
-    openPanel,
+    openPanel
 }
